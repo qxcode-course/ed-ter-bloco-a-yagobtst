12c1540f57e8f6fe1b88e1d618eaf27b5c6a7437
yagobtst <yagobatista07@hotmail.com>
2026-03-10 14:29:48 +0000

    sas

--- TKOED/fup/sobrou/go/draft.go
+++ TKOED/fup/sobrou/go/draft.go
@@ -4,5 +4,13 @@ func main() {
     var a, b, c int
     var d, e, f float64
     var g float64
-    
+
+    fmt.Scan(&a, &b, &c)
+    fmt.Scan(&d, &e, &f)
+    fmt.Scan(&g)
+
+    quantidade := (float64(a) * d) + (float64(b) * e) + (float64(c) * f)
+    troco := g - quantidade
+
+    fmt.Printf("%.2f\n", troco)
 }
