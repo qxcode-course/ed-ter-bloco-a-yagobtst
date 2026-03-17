3187407e722f3b24c39695e4c1a5715e5d1bb264
yago <yagobatista07@gmail.com>
2026-03-16 21:04:38 -0300

    a

--- tkoed/ed/baruel/go/draft.go
+++ tkoed/ed/baruel/go/draft.go
@@ -1,5 +1,53 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+    var total, qtd int
+    var v [100]int
+    var tem [51]int
+
+    fmt.Scan(&total)
+    fmt.Scan(&qtd)
+
+    for i := 0; i < qtd; i++ {
+        fmt.Scan(&v[i])
+        tem[v[i]]++
+    }
+
+    achou := false
+
+    for i := 1; i < qtd; i++ {
+        if v[i] == v[i-1] {
+            if achou {
+                fmt.Print(" ")
+            }
+            fmt.Print(v[i])
+            achou = true
+        }
+    }
+
+    if !achou {
+        fmt.Print("N")
+    }
+
+    fmt.Println()
+
+    achou = false
+
+    for i := 1; i <= total; i++ {
+        if tem[i] == 0 {
+            if achou {
+                fmt.Print(" ")
+            }
+            fmt.Print(i)
+            achou = true
+        }
+    }
+
+    if !achou {
+        fmt.Print("N")
+    }
+
+    fmt.Println()
 }
